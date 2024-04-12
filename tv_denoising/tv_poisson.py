353bcec703c5129c74b6929f43fbcfd5a2de57ae
Graham Pash <gtpash96@gmail.com>
2024-04-12 11:13:03 -0500

    only tv

--- tv_denoising/tv_poisson.py
+++ tv_denoising/tv_poisson.py
@@ -1,8 +1,7 @@
 import dolfin as dl
 import ufl
-import math
-import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 import sys
 import os
@@ -17,12 +16,6 @@ dl.set_log_active(False)
 from utils import parameter2NoisyObservations
 
 ## boundaries for the unit square
-def x_boundary(x, on_boundary):
-    return on_boundary and (x[0] < dl.DOLFIN_EPS or x[0] > 1.0 - dl.DOLFIN_EPS)
-
-def y_boundary(x, on_boundary):
-    return on_boundary and (x[1] < dl.DOLFIN_EPS or x[1] > 1.0 - dl.DOLFIN_EPS)
-
 def u0_boundary(x, on_boundary):
     return on_boundary
 
@@ -34,6 +27,9 @@ DIM = 2
 NOISE_LEVEL = 0.02
 ALPHA = 1e-2
 BETA = 1e-4
+PEPS = 0.5*ALPHA  # mass matrix scaled with TV
+MAX_ITER = 100
+CG_MAX_ITER = 75
 
 ## set up the mesh, mpi communicator, and function spaces
 mesh = dl.UnitSquareMesh(NX, NY)
@@ -58,7 +54,6 @@ bc = dl.DirichletBC(Vh[hp.STATE], zero, u0_boundary)  # homogeneous Dirichlet BC
 bc0 = bc  # same for the adjoint
 
 ## define the variational form
-
 def pde_varf(u,m,p):
     return ufl.exp(m)*ufl.inner(ufl.grad(u), ufl.grad(p))*ufl.dx - f*p*ufl.dx
     
@@ -68,38 +63,16 @@ pde = hp.PDEVariationalProblem(Vh, pde_varf, bc, bc0, is_fwd_linear=True)
 mtrue_exp = dl.Expression('1.0 + 7.0*(x[0]<=0.8)*(x[0]>=0.2)*(x[1]<=0.8)*(x[1]>=0.2)', degree=1)
 mtrue = dl.interpolate(mtrue_exp, Vh1)
 
-## set up observation operator for the top right corner
-xx = np.linspace(0.5, 1.0, 25, endpoint=False)
-xv, yv = np.meshgrid(xx, xx)
-targets = np.vstack([xv.ravel(), yv.ravel()]).T
-
-if rank == 0:
-    print(f"Number of observation points: {targets.shape[0]}")
+plt.figure()
+dl.plot(mtrue)
+plt.show()
 
 ## define observation operator
-B = hp.assemblePointwiseObservation(Vh[hp.STATE], targets)
 p2o = parameter2NoisyObservations(pde, mtrue.vector(), NOISE_LEVEL)
 p2o.generateNoisyObservations()
 
 ## generate synthetic observations
-# misfit = hp.DiscreteStateObservation(B, data, noise_std_dev**2)
-misfit = hp.ContinuousStateObservation(Vh=Vh[hp.STATE], dX=ufl.dx, bcs=[bc])
-misfit.d.axpy(1., p2o.noisy_data)
-misfit.noise_variance = p2o.noise_std_dev**2
-
-# smooth portion of the prior
-# same as: https://github.com/hippylib/hippylib/blob/master/applications/poisson/model_subsurf.py
-
-# anisotropic diffusion tensor
-theta0 = 2.
-theta1 = .5
-alpha  = math.pi/4
-anis_diff = dl.CompiledExpression(hp.ExpressionModule.AnisTensor2D(), degree = 1)
-anis_diff.set(theta0, theta1, alpha)
-
-gamma = .1
-delta = .5
-prior = hp.BiLaplacianPrior(Vh[hp.PARAMETER], gamma, delta, anis_diff, robin_bc=True)
+misfit = hp.ContinuousStateObservation(Vh=Vh[hp.STATE], dX=ufl.dx, data=p2o.noisy_data, noise_variance=p2o.noise_std_dev**2, bcs=[bc])
 
 # nonsmooth portion of the prior
 Vhm = Vh1
@@ -109,19 +82,28 @@ nsprior = hp.TVPrior(Vhm, Vhw, Vhwnorm, ALPHA, BETA)
 
 # set up the model describing the inverse problem
 TVonly = [True, False, True]
-model = hp.ModelNS(pde, misfit, prior, nsprior, which=TVonly)
-# model = hp.ModelNS(pde, misfit, prior, nsprior)
+model = hp.ModelNS(pde, misfit, None, nsprior, which=TVonly)
 
-m = prior.mean.copy()
+solver_params = hp.ReducedSpacePDNewtonCG_ParameterList()
+solver_params["max_iter"] = MAX_ITER
+solver_params["cg_max_iter"] = CG_MAX_ITER
+
+solver = hp.ReducedSpacePDNewtonCG(model, parameters=solver_params)
+
+# initial guess (zero)
+m = pde.generate_parameter()
 m.zero()
-solver = hp.ReducedSpacePDNewtonCG(model)
-# x = solver.solve([None, m, None, nsprior.compute_w(m)])
-x = solver.solve([None, m, None, None])
-print("Solver convergence criterion")
-print(solver.termination_reasons[solver.reason])
 
-# todo: add visualization
+# solve the system
+start = time.perf_counter()
+x = solver.solve([None, m, None, None])
+print(f"Time to solve: {(time.perf_counter()-start)/60:.2f} minutes")
+print(f"Solver convergence criterion:\t{solver.termination_reasons[solver.reason]}")
+
+# extract the solution and plot it
 xfunname = ["state", "parameter", "adjoint"]
 xfun = [hp.vector2Function(x[i], Vh[i], name=xfunname[i]) for i in range(len(Vh))]
 
-breakpoint()
+plt.figure()
+dl.plot(xfun[hp.PARAMETER])
+plt.show()
